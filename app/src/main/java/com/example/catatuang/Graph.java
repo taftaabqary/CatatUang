31732290c9a2bb87dc175e2ad45643ca3ac804a6
Muhammad Althaaf Abqary <taftaraharjo@gmail.com>
2022-12-01 21:33:42 +0700

    graph update

--- app/src/main/java/com/example/catatuang/Graph.java
+++ app/src/main/java/com/example/catatuang/Graph.java
@@ -10,22 +10,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import lecho.lib.hellocharts.model.PieChartData;
-import lecho.lib.hellocharts.model.SliceValue;
-import lecho.lib.hellocharts.view.PieChartView;
-
 public class Graph extends AppCompatActivity {
-
-    //deklarasi global
-    Cursor cursor;
-    PieChartView pieChartView;
     DatabaseHelper dbcenter;
     SessionManagement sessionManagement;
     Button back;
+    Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +32,7 @@ public class Graph extends AppCompatActivity {
         setContentView(R.layout.activity_graph);
 
         back = findViewById(R.id.back);
-        pieChartView = findViewById(R.id.chart);
+        PieChart pieChart = findViewById(R.id.pieChart);
         dbcenter = new DatabaseHelper(this);
 
         back.setOnClickListener(new View.OnClickListener() {//memberikan perintah jika button back di klik maka akan berpindah ke halaman menu
@@ -45,8 +44,7 @@ public class Graph extends AppCompatActivity {
             }
         });
 
-
-        List pieData = new ArrayList<>(); //membuat variabel pieData yang berupa arraylist untuk menyimpan isi dari chart
+        ArrayList<PieEntry> visitors = new ArrayList<>();
         sessionManagement = new SessionManagement(Graph.this);
         if (sessionManagement.isLoggedIn()) {
             final HashMap<String, String> user = sessionManagement.getUserInformation();
@@ -60,25 +58,30 @@ public class Graph extends AppCompatActivity {
                 int jumlah = Integer.parseInt(cursor.getString(0).toString());
                 String kategori = cursor.getString(1).toString();
                 //agar warna berbeda , buat if else sehingga memiliki warna beda tiap indeks cursor
-                if (cc == 0) {
-                    pieData.add(new SliceValue(jumlah, Color.BLUE).setLabel(kategori+jumlah)); //atur warna dan teks
-                } else if (cc == 1) {
-                    pieData.add(new SliceValue(jumlah, Color.GRAY).setLabel(kategori+jumlah));
-                } else if (cc == 2) {
-                    pieData.add(new SliceValue(jumlah, Color.RED).setLabel(kategori+jumlah));
-                } else if (cc == 3) {
-                    pieData.add(new SliceValue(jumlah, Color.MAGENTA).setLabel(kategori+jumlah));
-                } else if (cc == 4) {
-                    pieData.add(new SliceValue(jumlah, Color.CYAN).setLabel(kategori+jumlah));
+                if(cc == 0){
+                    visitors.add(new PieEntry(jumlah, kategori));
+                }else if (cc == 1){
+                    visitors.add(new PieEntry(jumlah, kategori));
+                }else if(cc == 2){
+                    visitors.add(new PieEntry(jumlah, kategori));
+                }else if(cc == 3){
+                    visitors.add(new PieEntry(jumlah, kategori));
+                }else if(cc == 4){
+                    visitors.add(new PieEntry(jumlah, kategori));
                 }
-
             }
-
-            PieChartData pieChartData = new PieChartData(pieData);
-            pieChartData.setHasLabels(true).setValueLabelTextSize(14);
-            pieChartData.setHasCenterCircle(true).setCenterText1("Income").setCenterText1FontSize(20).setCenterText1Color(Color.parseColor("#0097A7"));
-            pieChartView.setPieChartData(pieChartData);
         }
 
+        PieDataSet pieDataSet = new PieDataSet(visitors, "INCOME GRAPHIC");
+        pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        pieDataSet.setValueTextColor(Color.BLACK);
+        pieDataSet.setValueTextSize(16f);
+
+        PieData pieData = new PieData(pieDataSet);
+
+        pieChart.setData(pieData);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setCenterText("INCOME GRAPHIC BAR");
+        pieChart.animate();
     }
 }
--- app/src/main/java/com/example/catatuang/GraphExpenses.java
+++ app/src/main/java/com/example/catatuang/GraphExpenses.java
@@ -10,13 +10,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import lecho.lib.hellocharts.model.PieChartData;
-import lecho.lib.hellocharts.model.SliceValue;
 import lecho.lib.hellocharts.view.PieChartView;
+
 
 public class GraphExpenses extends AppCompatActivity {
     Cursor cursor;
@@ -28,27 +33,24 @@ public class GraphExpenses extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_graph);
+        setContentView(R.layout.activity_graph_expenses);
 
-        pieChartView = findViewById(R.id.chart);
-        dbcenter = new DatabaseHelper(this);
-        List pieData = new ArrayList<>();
         back = findViewById(R.id.back);
-        back.setOnClickListener(new View.OnClickListener() {
+        PieChart pieChart = findViewById(R.id.pieChart);
+        dbcenter = new DatabaseHelper(this);
+
+        back.setOnClickListener(new View.OnClickListener() {//memberikan perintah jika button back di klik maka akan berpindah ke halaman menu
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                Intent i = new Intent(getApplicationContext(), MainActivity.class); //berpindah ke halaman menu
                 startActivity(i);
             }
         });
+
+        ArrayList<PieEntry> visitors = new ArrayList<>();
         sessionManagement = new SessionManagement(GraphExpenses.this);
         if (sessionManagement.isLoggedIn()) {
             final HashMap<String, String> user = sessionManagement.getUserInformation();
-
-            // pieData.add(new SliceValue(15, Color.BLUE).setLabel("Q1: $10"));
-            // pieData.add(new SliceValue(25, Color.GRAY).setLabel("Q2: $4"));
-            //pieData.add(new SliceValue(10, Color.RED).setLabel("Q3: $18"));
-            // pieData.add(new SliceValue(60, Color.MAGENTA).setLabel("Q4: $28"));
 
             SQLiteDatabase db = dbcenter.getReadableDatabase();
             cursor = db.rawQuery("SELECT SUM(jumlah) as total, kategori FROM transaksi WHERE id_user = '" + user.get(sessionManagement.KEY_ID_USER) + "' and jenis = 'Expenses' group by kategori", null);
@@ -58,27 +60,31 @@ public class GraphExpenses extends AppCompatActivity {
                 cursor.moveToPosition(cc);
                 int jumlah = Integer.parseInt(cursor.getString(0).toString());
                 String kategori = cursor.getString(1).toString();
-                if (cc == 0) {
-                    pieData.add(new SliceValue(jumlah, Color.BLUE).setLabel(kategori+jumlah));
-                } else if (cc == 1) {
-                    pieData.add(new SliceValue(jumlah, Color.GRAY).setLabel(kategori+jumlah));
-                } else if (cc == 2) {
-                    pieData.add(new SliceValue(jumlah, Color.RED).setLabel(kategori+jumlah));
-                } else if (cc == 3) {
-                    pieData.add(new SliceValue(jumlah, Color.MAGENTA).setLabel(kategori+jumlah));
-                } else if (cc == 4) {
-                    pieData.add(new SliceValue(jumlah, Color.CYAN).setLabel(kategori+jumlah));
-                }else{
-                    pieData.add(new SliceValue(jumlah, Color.GREEN).setLabel(kategori+jumlah));
+                //agar warna berbeda , buat if else sehingga memiliki warna beda tiap indeks cursor
+                if(cc == 0){
+                    visitors.add(new PieEntry(jumlah, kategori));
+                }else if (cc == 1){
+                    visitors.add(new PieEntry(jumlah, kategori));
+                }else if(cc == 2){
+                    visitors.add(new PieEntry(jumlah, kategori));
+                }else if(cc == 3){
+                    visitors.add(new PieEntry(jumlah, kategori));
+                }else if(cc == 4){
+                    visitors.add(new PieEntry(jumlah, kategori));
                 }
-
             }
-
-            PieChartData pieChartData = new PieChartData(pieData);
-            pieChartData.setHasLabels(true).setValueLabelTextSize(14);
-            pieChartData.setHasCenterCircle(true).setCenterText1("Expenses").setCenterText1FontSize(20).setCenterText1Color(Color.parseColor("#0097A7"));
-            pieChartView.setPieChartData(pieChartData);
         }
 
+        PieDataSet pieDataSet = new PieDataSet(visitors, "INCOME GRAPHIC");
+        pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        pieDataSet.setValueTextColor(Color.BLACK);
+        pieDataSet.setValueTextSize(16f);
+
+        PieData pieData = new PieData(pieDataSet);
+
+        pieChart.setData(pieData);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setCenterText("INCOME GRAPHIC BAR");
+        pieChart.animate();
     }
 }
